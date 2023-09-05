40cafcc8836ff934342e4ad36c24d5c182048ae3
btwbeatriz <beatriz.ffreitas@hotmail.com>
2023-09-05 11:28:59 -0300

    Alterando a classe para RestController

--- src/main/java/br/com/fiap/mp/controller/HomeController.java
+++ src/main/java/br/com/fiap/mp/controller/HomeController.java
@@ -1,27 +1,27 @@
 package br.com.fiap.mp.controller;
 
 import br.com.fiap.mp.model.Pedido;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import br.com.fiap.mp.repository.PedidoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
-import java.util.Arrays;
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("/home")
 public class HomeController {
 
+    @Autowired
+    private PedidoRepository pedidoRepository;
+
     @GetMapping
-    public String home(Model model){
-        Pedido pedido = new Pedido();
-        pedido.setNomeProduto("Top Sundae Chocolate");
-        pedido.setUrlImagem("https://cache-backend-mcd.mcdonaldscupones.com/media/image/product$kpXGFtJB/200/200/original?country=br");
-        pedido.setUrlProduto("https://www.mcdonalds.com.br/cardapio/sobremesas/top-sundae-chocolate");
-        pedido.setDescricao("Sobremesa com Bebida láctea sabor baunilha e uma supercamada de cobertura sabor chocolate e ainda por cima com farofa de paçoca. O canudo completa a tentação.");
-        List<Pedido> pedidos = Arrays.asList(pedido);
-        model.addAttribute("pedidos", pedidos);
-        return "home";
+    public ModelAndView home(){
+        List<Pedido> pedidos = pedidoRepository.findAll();
+        ModelAndView mv = new ModelAndView("home");
+        mv.addObject("pedidos", pedidos);
+        return mv;
     }
 }
