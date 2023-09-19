c8c2dde0b19f80404f0d7ca7b30edd9ae26dc49b
btwbeatriz <beatriz.ffreitas@hotmail.com>
2023-09-19 11:20:11 -0300

    personalizando a mensagem de erro

--- src/main/java/br/com/fiap/mp/dto/RequisicaoNovoPedido.java
+++ src/main/java/br/com/fiap/mp/dto/RequisicaoNovoPedido.java
@@ -6,7 +6,7 @@ import jakarta.validation.constraints.Size;
 public record RequisicaoNovoPedido(
 
         @NotBlank
-        @Size(min = 5, max = 250)
+        @Size(min = 5, max = 250, message = "quantidade mínima de 5 e máxima de 250 caracteres")
         String nomeProduto,
         @NotBlank
         String urlProduto,
